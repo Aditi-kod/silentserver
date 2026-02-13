f284b05a9034781696218f7756d560c6b91123bb
Aditi Kumari <ad2019ti@gmail.com>
2026-02-13 23:07:43 +0530

    initial commit

--- utils/tokenGenerator.js
+++ utils/tokenGenerator.js
@@ -1,18 +1,13 @@
-const crypto = require("crypto");
-const { getSecretFromDB } = require("./mockDb");
-
 const generateToken = async (email) => {
-  try {
-    const secret = await getSecretFromDB();
+    try {
+        const secret = await getSecretFromDB();
 
-    return crypto
-      .createHmac("sha256", secret)
-      .update(email)
-      .digest("base64");
-  } catch (error) {
-    // THE BUG: Empty catch block.
-    // Error is swallowed and undefined is returned.
-  }
+        return crypto
+            .createHmac("sha256", secret)
+            .update(email)
+            .digest("base64");
+    } catch (error) {
+        console.error("Token generation failed:", error.message);
+        throw error;
+    }
 };
-
-module.exports = { generateToken };
